38f28c147619a6ff300a167cf58d569dccbb0bc2
Doro <dorobantu.daniel0@gmail.com>
2019-07-05 11:05:55 +0100

    2.1: course contents step6 exercise

---
+++ fullstack_part2/course_contents/src/Components/Content.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Part1 = props => {
+  return (
+    <p>
+      {props.parts[0].name} - {props.parts[0].exercises}
+    </p>
+  );
+};
+
+const Part2 = props => {
+  return (
+    <p>
+      {props.parts[1].name} - {props.parts[1].exercises}
+    </p>
+  );
+};
+
+const Part3 = props => {
+  return (
+    <p>
+      {props.parts[2].name} - {props.parts[2].exercises}
+    </p>
+  );
+};
+
+const Content = props => {
+  return (
+    <div>
+      <Part1 parts={props.course.parts} />
+      <Part2 parts={props.course.parts} />
+      <Part3 parts={props.course.parts} />
+    </div>
+  );
+};
+
+export default Content;
